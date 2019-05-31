d8c3e73df33c364b13f0ad8f6f74fc3c02427f1f
hguandl <hguandl@gmail.com>
2019-05-31 20:42:11 +0800

    vapoursynth-nnedi3cl

---
+++ vapoursynth-nnedi3cl.rb
@@ -0,0 +1,28 @@
+class VapoursynthNnedi3cl < Formula
+  desc "NNEDI3CL filter for VapourSynth"
+  homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-NNEDI3CL"
+  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-NNEDI3CL/archive/r7.3.tar.gz"
+  version "r7.3"
+  sha256 "01a162c036bd7f723c94160a820461fbde34cd3810194c273979cb0453b2d66b"
+
+  head do
+    url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-NNEDI3CL.git"
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "vapoursynth"
+
+  def install
+    system "meson build"
+    system "ninja -C build"
+    lib.install "build/libnnedi3cl.dylib"
+    lib.install "NNEDI3CL/nnedi3_weights.bin"
+  end
+
+  def post_install
+    (HOMEBREW_PREFIX/"lib/vapoursynth").install_symlink prefix/"lib/libnnedi3cl.dylib" => "libnnedi3cl.dylib"
+  end
+end
