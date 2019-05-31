2427fdd9c1d1b6a6c599f48cf9314c2b61b925a3
hguandl <hguandl@gmail.com>
2019-05-31 21:24:27 +0800

    flashk3yuu-deband: 2.0.0-1

--- flash3kyuu-deband.rb
+++ flash3kyuu-deband.rb
@@ -2,6 +2,8 @@ class Flash3kyuuDeband < Formula
   desc "A deband library and filter for avisynth/vapoursynth"
   homepage "https://github.com/SAPikachu/flash3kyuu_deband"
   url "https://github.com/SAPikachu/flash3kyuu_deband/archive/2.0.0-1.tar.gz"
+  version "2.0.0"
+  revision 1
   sha256 "5f68d017b45f8aecfeee3ac5343964c71eb8cbc209711aa5ad4dc9408f90f411"
 
   head do
