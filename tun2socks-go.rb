5bcb2cb113f6111d403a0b45e89ce86bdf44e45f
hguandl <hguandl@gmail.com>
2019-06-19 07:38:56 +0800

    tun2socks-go: add 1.16.1 bottle

--- tun2socks-go.rb
+++ tun2socks-go.rb
@@ -6,6 +6,7 @@ class Tun2socksGo < Formula
   head "https://github.com/eycorsican/go-tun2socks.git"
 
   bottle do
+    root_url "https://files.hguandl.com/bottles-custom"
     cellar :any_skip_relocation
     sha256 "9f8209f89a43fb983cb6d9f3485618ec6d901bd0268f285de6c583a8d0abb331" => :mojave
   end
