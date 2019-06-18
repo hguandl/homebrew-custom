7f181db02b10f5bb4c8eaed9ac9010d1cb462878
hguandl <hguandl@gmail.com>
2019-06-19 07:37:21 +0800

    tun2socks-go: add 1.16.1 bottle

--- tun2socks-go.rb
+++ tun2socks-go.rb
@@ -5,6 +5,11 @@ class Tun2socksGo < Formula
   sha256 "4a7178f75ea813e8ac49161ae6d8e004b848c4d1eccc44a432a908c1898ae138"
   head "https://github.com/eycorsican/go-tun2socks.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9f8209f89a43fb983cb6d9f3485618ec6d901bd0268f285de6c583a8d0abb331" => :mojave
+  end
+
   depends_on "go" => :build
 
   def install
