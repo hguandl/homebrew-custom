067e8d3cb41a4e4a558164dba0f2ad5b259968cb
Hao Guan <hguandl@gmail.com>
2019-07-06 13:42:52 +0800

    tun2socks-go: add vendors for go mod

--- tun2socks-go.rb
+++ tun2socks-go.rb
@@ -1,15 +1,19 @@
 class Tun2socksGo < Formula
   desc "A tun2socks implementation written in Go"
   homepage "https://github.com/eycorsican/go-tun2socks"
-  url "https://github.com/hguandl/go-tun2socks/archive/v1.16.2.tar.gz"
-  sha256 "648658875ee81ba6a1197b1c7c5e16ef2422e10537665fa918b4fbe57ee08f31"
-  revision 1
+  url "https://github.com/hguandl/go-tun2socks/releases/download/v1.16.2/go-tun2socks-1.16.2.tar.gz"
+  sha256 "147089b6fc038fe1b9a40c06711acacfed57a72b39ae1ab7191b3078b010bdc1"
+  revision 2
   head "https://github.com/hguandl/go-tun2socks.git"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+
+    # Use vendor for released code tarball
+    if build.stable?
+      ENV["GOFLAGS"] = "#{ENV["GOFLAGS"]} -mod=vendor"
 
     # Version info independent from git
     inreplace "Makefile", "$(shell git describe --tags)", "v#{version}"
