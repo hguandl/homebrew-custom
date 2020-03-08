07aa67a95e29de4b7db2603af544c1c46e5949e1
Hao Guan <hguandl@gmail.com>
2020-03-08 18:07:00 +0800

    update terminate condition

--- precondition.sh
+++ precondition.sh
@@ -4,18 +4,18 @@ export TARGET_FORMULA=$(echo ${TRAVIS_COMMIT_MESSAGE} | cut -f 1 -d ":")
 
 if [[ "${TRAVIS_COMMIT_MESSAGE}" == *": add"*"bottle." ]]; then
   echo "Bottle added, skip building."
-  exit 1
+  exit 0
 fi
 
 if [[ "${TRAVIS_COMMIT_MESSAGE}" != *":"* ]]; then
   echo "No formula name."
-  exit 1
+  exit 0
 fi
 
 if [ ! ${TARGET_FORMULA} ]; then
   echo "Empty formula name."
-  exit 1
+  exit 0
 fi
 
 echo "Would build ${TARGET_FORMULA}."
-exit 0
+export DO_BUILD=1
