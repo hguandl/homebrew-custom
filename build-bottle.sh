2b0bd0113b2e4fb584eff3ce192263b7f8e6c120
Hao Guan <hguandl@gmail.com>
2020-03-08 15:47:00 +0800

    exclude needless building

--- build-bottle.sh
+++ build-bottle.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ${TRAVIS_COMMIT_MESSAGE} == "*add*" ] && [ ${TRAVIS_COMMIT_MESSAGE} == "*bottle." ]; then
+  exit 0
+fi
+
+if [ ${TRAVIS_COMMIT_MESSAGE} != "*:*" ]; then
+  exit 0
+fi
+
 if [ ! ${TARGET_FORMULA} ]; then
   exit 0
 fi
